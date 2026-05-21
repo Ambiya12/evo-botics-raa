68bdd8cd963b260c36d7354faf88aa3272f02992
Anatole2 <anatole.dupuis18@gmail.com>
2026-05-21 14:07:23 +0200

    Booking implementation

--- reservationApp/routes/api.php
+++ reservationApp/routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\SessionController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/reservations/validate', [ReservationController::class, 'validateQRCode']);
+Route::post('/sessions/book', [SessionController::class, 'bookSession']);
