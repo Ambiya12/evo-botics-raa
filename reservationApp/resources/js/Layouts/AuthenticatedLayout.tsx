2a1400f484f34ffeaa8adeb30beabc619e5f0845
Jules Bourin <164096536+NotUndefind@users.noreply.github.com>
2026-06-16 15:53:54 +0200

    feat: protect dashboard route with user middelware (#14)

--- reservationApp/resources/js/Layouts/AuthenticatedLayout.tsx
+++ reservationApp/resources/js/Layouts/AuthenticatedLayout.tsx
@@ -51,12 +51,14 @@ export default function AuthenticatedLayout({ header, children }) {
                             </div>
 
                             <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                                <NavLink
-                                    href={route('dashboard')}
-                                    active={route().current('dashboard')}
-                                >
-                                    {t('Dashboard')}
-                                </NavLink>
+                                {!isAdmin && (
+                                    <NavLink
+                                        href={route('dashboard')}
+                                        active={route().current('dashboard')}
+                                    >
+                                        {t('Dashboard')}
+                                    </NavLink>
+                                )}
                                 {isAdmin && (
                                     <>
                                         <NavLink
@@ -193,12 +195,14 @@ export default function AuthenticatedLayout({ header, children }) {
                     }
                 >
                     <div className="space-y-1 pb-3 pt-2">
-                        <ResponsiveNavLink
-                            href={route('dashboard')}
-                            active={route().current('dashboard')}
-                        >
-                            {t('Dashboard')}
-                        </ResponsiveNavLink>
+                        {!isAdmin && (
+                            <ResponsiveNavLink
+                                href={route('dashboard')}
+                                active={route().current('dashboard')}
+                            >
+                                {t('Dashboard')}
+                            </ResponsiveNavLink>
+                        )}
                         {isAdmin && (
                             <>
                                 <ResponsiveNavLink
