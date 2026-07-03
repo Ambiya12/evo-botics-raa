6f2f8ac0bdd0a6495569a3e18a0d440d35eefe60
antoine <antoinetu2003@gmail.com>
2026-07-03 21:18:46 +0200

    Fix: unused routes

--- reservationApp/routes/web.php
+++ reservationApp/routes/web.php
@@ -40,8 +40,6 @@ Route::middleware(['auth', 'verified', 'not-admin'])->group(function () {
         return Inertia::render('Dashboard', [
             'reservations' => auth()->user()->reservations()->with('bookingSession.room')->latest()->get(),
         ]);
-        Route::delete('/reservations/{uuid}', [ReservationController::class, 'cancel'])->name('reservations.cancel');
-        Route::post('/sessions/book', [BookingSessionController::class, 'bookSession']);
     })->name('dashboard');
 
     Route::delete('/reservations/{uuid}', [ReservationController::class, 'cancel'])->name('reservations.cancel');
