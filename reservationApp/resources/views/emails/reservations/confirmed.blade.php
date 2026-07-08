2d932dcfe4ffdc6bdbe9bf34d3328e6ce09fa859
antoine <antoinetu2003@gmail.com>
2026-07-08 09:15:22 +0200

    Fix: remove btn

--- reservationApp/resources/views/emails/reservations/confirmed.blade.php
+++ reservationApp/resources/views/emails/reservations/confirmed.blade.php
@@ -13,11 +13,7 @@ Your room is reserved for **{{ $reservation->bookingSession->date->format('Y/m/d
 		<img src="data:image/png;base64,{{ $qrCode }}" alt="QR Code" style="width: 200px; height: 200px;">
 		<p style="color: #9ca3af; font-size: 12px; margin-top: 8px;">If the image does not display, the QR code is also attached as a file.</p>
 	</div>
-{{--
-<x-mail::button :url="config('app.url')">
-View my reservation
-</x-mail::button>
---}}
+	
 See you soon,<br>
 {{ config('app.name') }}
 </x-mail::message>
