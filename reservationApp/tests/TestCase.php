ede806ffb1f568390bdeabe3691d7b40758aeebc
Galystann Ambiya <73895442+Ambiya12@users.noreply.github.com>
2026-07-17 01:19:18 +0200

    chore: prepare Evo-Botics v0.1.0 release (#35)
    * chore: prepare v0.1.0 release

--- reservationApp/tests/TestCase.php
+++ reservationApp/tests/TestCase.php
@@ -6,5 +6,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Frontend compilation is validated by a separate CI job. Feature
+        // tests should render Blade views without requiring a Vite manifest.
+        $this->withoutVite();
+    }
 }
